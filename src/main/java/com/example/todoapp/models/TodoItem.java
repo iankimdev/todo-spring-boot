15bcd9d88c0c8f80b762c2b96992f614bdd61aef
ian <ian.kim.dev@gmail.com>
2023-06-14 10:47:49 +0900

    feat(models): Add models

---
+++ src/main/java/com/example/todoapp/models/TodoItem.java
@@ -0,0 +1,53 @@
+package com.example.todoapp.models;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+import java.time.Instant;
+
+@Entity
+@Table(name = "todo_item")
+public class TodoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @Getter
+    @Setter
+    private boolean complete;
+
+    @Getter
+    @Setter
+    private Instant createdDate;
+
+    @Getter
+    @Setter
+    private Instant modifiedDate;
+
+    public TodoItem() {}
+
+    public TodoItem(String description) {
+        this.description = description;
+        this.complete = false;
+        this.createdDate = Instant.now();
+        this.modifiedDate = Instant.now();
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("TodoItem{id=%d, description='%s', complete='%s', createdDate='%s', modifiedDate='%s'}",
+        id, description, complete, createdDate, modifiedDate);
+    }
+}
