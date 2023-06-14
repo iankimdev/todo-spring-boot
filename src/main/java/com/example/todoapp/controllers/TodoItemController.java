7c2ba160beeb29a32640d6860d94c3401b2a3f77
ian <ian.kim.dev@gmail.com>
2023-06-14 15:21:59 +0900

    refactor(controllers): Add @controller annotation

--- src/main/java/com/example/todoapp/controllers/TodoItemController.java
+++ src/main/java/com/example/todoapp/controllers/TodoItemController.java
@@ -1,28 +1,26 @@
 package com.example.todoapp.controllers;
 
-import java.time.Instant;
-import java.time.ZoneId;
-
-import com.example.todoapp.repositories.*;
+import com.example.todoapp.repositories.TodoItemRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-
+import org.springframework.stereotype.Controller;
+@Controller
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
+    
     @Autowired
     private TodoItemRepository todoItemRepository;
-    
+
     @GetMapping("/")
     public ModelAndView index() {
         logger.info("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("todoItems", todoItemRepository.findAll());
-        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
+        System.out.println(todoItemRepository);
         return modelAndView;
     }
 }   
