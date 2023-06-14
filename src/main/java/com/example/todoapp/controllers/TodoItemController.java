de7e8c213614d73e1cc51f8acaaf99929200bc69
ian <ian.kim.dev@gmail.com>
2023-06-14 10:58:27 +0900

    feat(controllers): Add retrieval ot todo itmes and day

--- src/main/java/com/example/todoapp/controllers/TodoItemController.java
+++ src/main/java/com/example/todoapp/controllers/TodoItemController.java
@@ -1,17 +1,28 @@
 package com.example.todoapp.controllers;
 
+import java.time.Instant;
+import java.time.ZoneId;
+
+import com.example.todoapp.repositories.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
-
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+    
     @GetMapping("/")
     public ModelAndView index() {
         logger.info("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
+        modelAndView.addObject("today", Instant.now().atZone(ZoneId.systemDefault()).toLocalDate().getDayOfWeek());
         return modelAndView;
     }
 }   
