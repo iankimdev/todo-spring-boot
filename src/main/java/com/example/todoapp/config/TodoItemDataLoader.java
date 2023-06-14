110a426e504ffc0c5595369b2ac0386e9ae4db5a
ian <ian.kim.dev@gmail.com>
2023-06-14 10:49:06 +0900

    feat(config): Add data load configuration

---
+++ src/main/java/com/example/todoapp/config/TodoItemDataLoader.java
@@ -0,0 +1,35 @@
+package com.example.todoapp.config;
+
+import com.example.todoapp.models.*;
+import com.example.todoapp.repositories.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TodoItemDataLoader implements CommandLineRunner{
+    private final Logger logger = LoggerFactory.getLogger(TodoItemDataLoader.class);
+    
+    @Autowired
+    TodoItemRepository todoItemRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadSeedData();
+    }
+
+    private void loadSeedData() {
+        if (todoItemRepository.count() == 0) {
+            TodoItem todoItem1 = new TodoItem("todo 1");
+            TodoItem todoItem2 = new TodoItem("todo 2");
+
+            todoItemRepository.save(todoItem1);
+            todoItemRepository.save(todoItem2); 
+        }
+
+        logger.info("Number of TodoItems: {}", todoItemRepository.count());
+    }
+}
