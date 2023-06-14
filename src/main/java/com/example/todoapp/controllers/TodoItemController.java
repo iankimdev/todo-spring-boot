19f11323e1630e92852fd6f7705c647fad8b6277
ian <ian.kim.dev@gmail.com>
2023-06-14 16:38:57 +0900

    feat(controllers): Add update controller

--- src/main/java/com/example/todoapp/controllers/TodoItemController.java
+++ src/main/java/com/example/todoapp/controllers/TodoItemController.java
@@ -1,13 +1,23 @@
 package com.example.todoapp.controllers;
 
+import com.example.todoapp.models.TodoItem;
 import com.example.todoapp.repositories.TodoItemRepository;
+
+import java.time.Instant;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+
 @Controller
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
@@ -22,5 +32,17 @@ public class TodoItemController {
         modelAndView.addObject("todoItems", todoItemRepository.findAll());
         System.out.println(todoItemRepository);
         return modelAndView;
+    }
+
+    @PostMapping("/todo/{id}")
+    public String updateTodoItem(@PathVariable("id") long id, @Valid TodoItem todoItem, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item";
+        }
+
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
     }
 }   
